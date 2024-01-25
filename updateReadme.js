3b1e32e324fe87a24b0a44ce5750e495e8597894
Tommy TSWD <54892623+ThomasTSWD@users.noreply.github.com>
2024-01-25 17:05:40 +0100

    Update updateReadme.js

--- updateReadme.js
+++ updateReadme.js
@@ -1,4 +1,3 @@
-const fetch = require('node-fetch');
 const fs = require('fs');
 
 async function getRandomJoke() {
@@ -7,9 +6,9 @@ async function getRandomJoke() {
     const data = await response.json();
 
     if (data.type === "single") {
-      return `**Blague du jour :** ${data.joke}`;
+      return `Blague du jour : ${data.joke}`;
     } else {
-      return `**Blague du jour :**\n\n*${data.setup}*\n\n*${data.delivery}*`;
+      return `Blague du jour :\n\n${data.setup}\n\n${data.delivery}`;
     }
   } catch (error) {
     console.error("Erreur lors de la récupération de la blague :", error);
@@ -21,14 +20,23 @@ async function updateReadmeWithJoke() {
   try {
     const jokeData = await getRandomJoke();
 
+    // Vérifie la réponse de l'API
+    if (!jokeData) {
+      console.error("Réponse de l'API invalide ou vide.");
+      return;
+    }
+
+    // Lit le README
     const readmePath = 'README.md';
     const readmeData = fs.readFileSync(readmePath, 'utf-8');
 
+    // Remplace la section de blague dans le README
     const updatedReadme = readmeData.replace(
       /<!-- START_JOKE_SECTION -->[\s\S]*<!-- END_JOKE_SECTION -->/gim,
       `<!-- START_JOKE_SECTION -->\n${jokeData}\n<!-- END_JOKE_SECTION -->`
     );
 
+    // Écriture du nouveau README
     fs.writeFileSync(readmePath, updatedReadme, 'utf-8');
 
     console.log('README mis à jour avec la nouvelle blague.');
@@ -36,4 +44,6 @@ async function updateReadmeWithJoke() {
     console.error("Erreur lors de la mise à jour du README :", error);
   }
 }
+
+// Appelle la fonction pour mettre à jour le README avec la nouvelle blague
 updateReadmeWithJoke();

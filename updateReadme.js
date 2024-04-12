bfd14ce9cfef6a4164bd33080b15c3688092986b
Tommy TSWD <54892623+ThomasTSWD@users.noreply.github.com>
2024-04-12 13:29:54 +0200

    Being serious

--- updateReadme.js
+++ updateReadme.js
@@ -1,50 +1,43 @@
 const fs = require('fs');
 const fetch = require('node-fetch');
 
-async function getRandomJoke() {
+async function getRandomQuote() {
   try {
-    const response = await fetch("https://sv443.net/jokeapi/v2/joke/Any?blacklistFlags=religious,political");
+    const response = await fetch("http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en");
     const data = await response.json();
 
-    if (data.type === "single") {
-      return `> ${data.joke}`;
-    } else {
-      return `> \n> ${data.setup}\n> \n> ${data.delivery}`;
-    }
+    return `> ${data.quoteText}\n> \n> - ${data.quoteAuthor}`;
   } catch (error) {
-    console.error("Erreur lors de la récupération de la blague :", error);
-    return "Erreur lors de la récupération de la blague.";
+    console.error("Erreur lors de la récupération de la citation :", error);
+    return "Erreur lors de la récupération de la citation.";
   }
 }
 
-async function updateReadmeWithJoke() {
+async function updateReadmeWithQuote() {
   try {
-    const jokeData = await getRandomJoke();
+    const quoteData = await getRandomQuote();
 
-    // Vérifie la réponse de l'API
-    if (!jokeData) {
+
+    if (!quoteData) {
       console.error("Réponse de l'API invalide ou vide.");
       return;
     }
 
-    // Lit le README
+    // read README
     const readmePath = 'README.md';
     const readmeData = fs.readFileSync(readmePath, 'utf-8');
 
-    // Remplace la section de blague dans le README
     const updatedReadme = readmeData.replace(
       /<!-- START_JOKE_SECTION -->[\s\S]*<!-- END_JOKE_SECTION -->/gim,
-      `<!-- START_JOKE_SECTION -->\n${jokeData}\n<!-- END_JOKE_SECTION -->`
+      `<!-- START_JOKE_SECTION -->\n${quoteData}\n<!-- END_JOKE_SECTION -->`
     );
 
-    // Écriture du nouveau README
     fs.writeFileSync(readmePath, updatedReadme, 'utf-8');
 
-    console.log('README mis à jour avec la nouvelle blague.');
+    console.log('README mis à jour avec la nouvelle citation.');
   } catch (error) {
     console.error("Erreur lors de la mise à jour du README :", error);
   }
 }
 
-// Appelle la fonction pour mettre à jour le README avec la nouvelle blague
-updateReadmeWithJoke();
+updateReadmeWithQuote();

c52f13f5c45662cc4bb9ccfaf3eaa91903fbdecf
ThomasTSWD <hello@tswd.fr>
2026-04-25 18:25:29 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+![header](https://raw.githubusercontent.com/ThomasTSWD/ThomasTSWD/master/img/edit_moddedfull.gif)
+
+### Hi there 👋
+
+Web developer, webdesigner and webmaster with 8+ years of experience in freelance and agency environments. Currently transitioning toward DevOps and automation, focused on helping businesses optimize workflows and improve efficiency through modern tools and processes ...
+
+-----
+
+**Weekly quote updated every saturday with Github Actions 💁‍♂️**
+
+
+<!-- START_JOKE_SECTION -->
+> Our virtues and our failings are inseparable, like force and matter. When they separate, man is no more.  
+> 
+> - Nikola Tesla 
+<!-- END_JOKE_SECTION -->
+
+
+*Thanks to Forismatic API*
+
+
+
+![Build README](https://github.com/ThomasTSWD/ThomasTSWD/workflows/Build%20README/badge.svg)
+
---
+++ updateReadme.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+
+async function getRandomQuote() {
+    try {
+        const response = await fetch("http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en");
+        const data = await response.json();
+
+        return `> ${data.quoteText}\n> \n> - ${data.quoteAuthor}`;
+    } catch (error) {
+        console.error("Erreur lors de la récupération de la citation :", error);
+        return "Erreur lors de la récupération de la citation.";
+    }
+}
+
+async function updateReadmeWithQuote() {
+    try {
+        const quoteData = await getRandomQuote();
+
+
+        if (!quoteData) {
+            console.error("Réponse de l'API invalide ou vide.");
+            return;
+        }
+
+        // read README
+        const readmePath = 'README.md';
+        const readmeData = fs.readFileSync(readmePath, 'utf-8');
+
+        const updatedReadme = readmeData.replace(
+            /<!-- START_JOKE_SECTION -->[\s\S]*<!-- END_JOKE_SECTION -->/gim,
+            `<!-- START_JOKE_SECTION -->\n${quoteData}\n<!-- END_JOKE_SECTION -->`
+        );
+
+        fs.writeFileSync(readmePath, updatedReadme, 'utf-8');
+
+        console.log('README mis à jour avec la nouvelle citation.');
+    } catch (error) {
+        console.error("Erreur lors de la mise à jour du README :", error);
+    }
+}
+
+updateReadmeWithQuote();
